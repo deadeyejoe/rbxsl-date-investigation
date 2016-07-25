4ffc92cd562ccf66ad41bf0b48861d8b23e5c652
Joe Douglas <joe@bccriskadvisory.com>
2016-07-25 17:14:58 +0100

    Can run for a set time

--- app/workers/date_worker.rb
+++ app/workers/date_worker.rb
@@ -6,12 +6,10 @@ class DateWorker
   def perform
     @start_time = Time.zone.now
 
-    if FIXED_TIME + 1.hour < Time.zone.now
-      logger.info "The app was started over an hour ago"
-    end
+    num = Random.new.rand(12)
 
-    if @start_time < Time.zone.now + 1.minute
-      logger.info "The first rule of tautology club is the first rule of tautology club!"
+    if FIXED_TIME + num.hours < Time.zone.now
+      logger.info "The app was started over #{num} hour(s) ago"
     end
   rescue ArgumentError => e
     logger.error "An argument error has occurred: #{e}"
--- app/workers/multi_worker.rb
+++ app/workers/multi_worker.rb
@@ -1,16 +1,36 @@
 class MultiWorker
   include Sidekiq::Worker
 
-  def perform threads, workers
-    rng = Random.new
+  READ_TIME = Time.zone.now
 
-    threads.times do
+  def perform duration
+    @rng = Random.new
+
+    100.times do
       Thread.new do
-        workers.times do
-          DateWorker.perform_async
-          sleep rng.rand(0.01)
+        100.times do
+          start_worker
         end
       end
+    end
+
+    reschedule duration
+  end
+
+  def start_worker
+    Time.zone.now + @rng.rand(100)
+
+    DateWorker.perform_async
+
+    sleep @rng.rand(0.01)
+  rescue ArgumentError => e
+    logger.error "An argument error occurred! #{e}"
+    logger.error e.backtrace.join "\n"
+  end
+
+  def reschedule duration
+    unless READ_TIME + duration.seconds < Time.zone.now
+      self.class.perform_in(@rng.rand(120).seconds, duration)
     end
   end
 end
